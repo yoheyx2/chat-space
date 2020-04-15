e2866ba910a25377f5695408eb02e7d6ba767c8f
yoheyx2 <62735469+yoheyx2@users.noreply.github.com>
2020-04-15 20:54:48 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,6 @@ set :keep_releases, 5
 # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
 
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
